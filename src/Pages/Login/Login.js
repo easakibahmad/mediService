ed056856b2700ea96cf6bb348baf207ba4231863
easakibahmad <sakib.sgc.du@gmail.com>
2023-03-21 17:42:20 +0600

    login page created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const handleLogin = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="m-16 md:w-1/3  w-2/3 mx-auto">
       <h2 className="text-3xl font-bold text-center">Login Now</h2>
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(handleLogin)}
         className="grid grid-cols-1 gap-3"
       >
         <div className="form-control w-full">
@@ -37,9 +40,21 @@ const Login = () => {
             className="input input-bordered w-full rounded"
             {...register("password")}
           />
+          <label className="label">
+            <span className="label-text">Forget password?</span>
+          </label>
         </div>
-        <input value="login" type="submit" />
+        <input className="btn btn-accent rounded" value="Login" type="submit" />
       </form>
+      <p className="mt-3">
+        new to mediService? please{" "}
+        <Link className="text-purple-500" to="/signup">
+          signup
+        </Link>{" "}
+        first!!
+      </p>
+      <div className="divider">OR</div>
+      <div className="btn btn-accent w-full rounded">continue with google</div>
     </div>
   );
 };
