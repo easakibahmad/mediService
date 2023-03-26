5f4fb770525c1be3077876bd76c6da2b2593adab
easakibahmad <sakib.sgc.du@gmail.com>
2023-03-26 17:40:51 +0600

    footer and appointment page modified

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import { DayPicker } from "react-day-picker";
 import { format } from "date-fns";
 import appointment from "../../../assets/images/duMedical.jpeg";
+import medicalTeam from "../../../assets/images/doctorsServices.png";
 
 const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
   return (
@@ -14,7 +15,12 @@ const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
       }}
     >
       <div className="hero pt-8 pb-4">
-        <div className="hero-content  text-md flex-col lg:flex-row-reverse">
+        <div className="hero-content  text-md flex-col md:flex-row-reverse">
+          <img
+            src={medicalTeam}
+            alt="medicalTeam"
+            className="md:block hidden lg:w-1/2 h-52"
+          />
           <div
             style={{
               color: "#184C4f",
@@ -22,7 +28,7 @@ const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
             }}
           >
             <DayPicker
-              className=" py-2 px-3 "
+              className=" py-1 px-3 "
               mode="single"
               selected={selectedDate}
               onSelect={setSelectedDate}
